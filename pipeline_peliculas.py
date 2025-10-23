f03cc9ce3e9a0a1f01b106c099d7b91788e20bdd
Santiago Borja <santiago.borja@gmail.com>
2025-10-23 02:49:21 -0500

    FEAT: Trabajando con la data del proyecto, primeras muestras

--- pipeline_peliculas.py
+++ pipeline_peliculas.py
@@ -1,27 +1,38 @@
 import pandas as pd
 import numpy as np
-import os
 from pipeline.data_loader import cargar_datos
 from pipeline.data_process import procesar_ratings, procesar_movie
 
 if __name__ == "__main__":
+    # --------------------------------------
+    # Lista de archivo
+    # --------------------------------------
     files = ["movie.csv", "tag.csv", "rating.csv"]
-    # files = ["rating.csv"]
+
     # --------------------------------------
     # Carga de datos CSV
     # --------------------------------------
     dict_df = cargar_datos(files)
     
+    # --------------------------------------
     # Procesamiento de la informacion
     # --------------------------------------
+    
     # Procesando Movies
     df_movie = procesar_movie(dict_df[files[0]])
     # Procesando Rating
     df_rating_general, dim_rating = procesar_ratings(dict_df[files[2]])
     
+    
+    # --------------------------------------
+    # Uniones
+    # --------------------------------------
     tabla_hecho = pd.merge(df_movie, df_rating_general, on="movieid", how="left")
     
-    print("TABLA DE HECHO")
+    # --------------------------------------
+    # Resultados
+    # --------------------------------------
+    print("TABLA DE HECHO `h_movie`")
     print(tabla_hecho.head())
     
     print("TABLA DIMENSION `d_rating`")
