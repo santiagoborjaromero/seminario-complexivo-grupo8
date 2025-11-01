2d5e29676477763ed7b39eba66b5ce026c495840
Hugo Herrera - Hugueins <hugo.herrera.villalva@gmail.com>
2025-11-01 16:59:49 -0500

    feat: Carga de 'procesados_movies.csv' con caché

--- main_dashboard.py
+++ main_dashboard.py
@@ -10,3 +10,23 @@ st.set_page_config(
 )
 
 st.title(" 🔥 Dashboard Recomendacíon Hibrida de Películas 🔥 ")
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # Define la ruta del archivo procesado
+DATA_PROCESS_DIR = os.path.join(BASE_DIR, 'data', 'process')
+PROCESSED_FILE = os.path.join(DATA_PROCESS_DIR, 'procesados_movies.csv')
+
+
+@st.cache_data 
+def load_data(file_path):
+    """
+    'load_data' carga el archivo CSV procesado del pipeline usando
+     @st.cache_data para optimizar el rendimiento, para que Streamlit no lea el archivo en cada click.
+    """
+    try:
+        df = pd.read_csv(file_path, encoding='latin1') 
+        return df
+    except FileNotFoundError:
+        st.error(f"Error: No se encontró el archivo 'procesados_movies.csv' en {file_path}")
+        st.info("Por favor, ejecuta primero python main_pipeline.py para generar el archivo de datos procesados.")
+        return None
