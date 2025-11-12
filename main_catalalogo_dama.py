8bf2a93559f317d2670cb29472413614f0c2c6eb
Santiago Borja <santiago.borja@gmail.com>
2025-11-12 12:15:27 -0500

    FEAT: mensaje de informacion al usuario

--- main_catalalogo_dama.py
+++ main_catalalogo_dama.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
+print(f"Catálogo DAMA - Contruyendo la data para catalogo")
 columnas = ["dama"]
 registros = ["Movie", "Tag", "Rating"]
 
+print(f"Catálogo DAMA - Creando Dataframe")
 df = pd.DataFrame(registros, columns=columnas)
+
+print(f"Catálogo DAMA - Guardando data/process/catalog_Dama.csv")
 df.to_csv("./data/process/catalog_Dama.csv", index=False)
