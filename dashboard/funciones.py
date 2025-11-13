a00636b5231ada0b365aff9b50fb9e73d933fa01
Santiago Borja <santiago.borja@gmail.com>
2025-11-13 10:02:12 -0500

    FIX: modificaciones

--- dashboard/funciones.py
+++ dashboard/funciones.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import os
 import requests
-import math 
 #---------------------------------------------------------
 # Define las rutas a los archivos de datos procesados
 #---------------------------------------------------------
