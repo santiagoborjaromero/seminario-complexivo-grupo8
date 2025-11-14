111f5bb4ce548d2baaf1c6eb037655d5f39b11e0
Santiago Borja <santiago.borja@gmail.com>
2025-11-14 16:10:48 -0500

    FIX: cambio de contantes a variables

--- api/utils/funciones.py
+++ api/utils/funciones.py
@@ -2,13 +2,13 @@ import os
 import pandas as pd
 
 def load_data(file_path):
-    BASE_DIR = os.getcwd() 
-    DATA_PROCESS_DIR = os.path.join(BASE_DIR, 'data', 'process')
-    PROCESSED_FILE = os.path.join(DATA_PROCESS_DIR, file_path)
+    base_dir = os.getcwd() 
+    data_process_dir = os.path.join(base_dir, 'data', 'process')
+    processed_file = os.path.join(data_process_dir, file_path)
     # print(PROCESSED_FILE)
     try:
-        df = pd.read_csv(PROCESSED_FILE, encoding='utf8')
+        df = pd.read_csv(processed_file, encoding='utf8')
         return df
     except FileNotFoundError:
-        print(f"Error: No se encontró el archivo {PROCESSED_FILE}")
+        print(f"Error: No se encontró el archivo {processed_file}")
         return None
